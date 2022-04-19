9886ee242aa4fa8ff277570553d942ba8ce5182c
Éric Almeida <ericalmeida.sp@gmail.com>
2022-04-19 10:08:44 -0300

    test

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 import { Application } from "https://deno.land/x/oak@v10.5.1/mod.ts";
-import { oakCors } from "../../mod.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
 import { router } from "./routes.ts";
 
 export const app = new Application();
